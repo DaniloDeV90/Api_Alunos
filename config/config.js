e6a386d4b30cb1468990e03e7d43484e994e6eeb
DaniloDeV90 <duhflores78@gmail.com>
2023-08-16 16:48:31 -0300

    sad

--- config/config.js
+++ config/config.js
@@ -2,17 +2,24 @@ require ("dotenv").config ();
 
 
 module.exports = {
-    dialect: process.env.DATABASE_NAME_DATA,
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    host: "ep-ancient-breeze-539214.us-east-2.aws.neon.tech",
+    dialect: "postgres",
+    
+    port:  5432,
+    username: "DaniloDeV90",
+    password:"IK3F5ohOucaj",
+    database: "bd_Alunos",
     define: {
         timesStamps: true
     },
     dialectOptions: {
-        connectTimeout: 60000
+        connectTimeout: 60000,
+        ssl: true,
+        sslmode: "require"
       },
+     
 }
+
+
+
 
